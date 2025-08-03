e8132eaf138307560f67c1c924d3fb451b373c09
Yatharth Sanghavi <141222209+YatharthSanghavi@users.noreply.github.com>
2025-08-03 11:08:10 +0530

    Create lab_3.sql

---
+++ lab_3.sql
@@ -0,0 +1,15 @@
+--Lab => 3
+--Perform SQL queries for Like operator
+
+
+--Part => A
+
+create table student
+(
+ stuid int,
+ firstname varchar(25),
+ lastname varchar(25),
+ website varchar(50),
+ city varchar(25),
+ address varchar(100)
+)
