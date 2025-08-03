128691ada3f0b2cf6ed6e9af955dd6cd9d55972f
Yatharth Sanghavi <141222209+YatharthSanghavi@users.noreply.github.com>
2025-08-03 14:24:39 +0530

    Update lab_3.sql

--- lab_3.sql
+++ lab_3.sql
@@ -13,3 +13,14 @@ create table student
  city varchar(25),
  address varchar(100)
 )
+
+insert into student values
+(1011,'keyur','patel','techonthenet.com','rajkot','A-303 Vasant Kunj, Rajkot'),
+(1022,'hardik','shah','digminecraft.com','ahmedabad','Ram Krupa, Raiya Road'),
+(1033,'kajal','trivedi','bigactivitiess.com','baroda','raj bhavan plot, near garden'),
+(1044,'bhoomi','gajera','checkyourmath.com','ahmedabad','Jigs Home, Narol'),
+(1055,'harmit','mital','@me.darshan.com','rajkot','b-55,raj residency'),
+(1066,'ashok','jani',null,'baroda','a502,club house buliding');
+
+
+--1. Display the name of students whose name starts with ‘k’
